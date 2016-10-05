67f620bcd3538b6d3f92caa289b564adc9582cba
Yaroslav Liakh <liakh_ya@mail.ru>
2016-10-05 21:50:47 +0300

    header ready

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,6 @@
 /**
  * Created by Ярик on 04.10.2016.
  */
+$(document).ready(function () {
+    //$('.header-contact-allNumber').find('label:after').css({'backgroundPosition': '-3px 11px', 'backgroundColor':'aqua'});
+});
