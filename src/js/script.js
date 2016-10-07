3b52abacb477ef3e6a388cf0f7e810ea58d97356
Yaroslav Liakh <liakh_ya@mail.ru>
2016-10-07 15:16:30 +0300

    dropdown phone number

--- src/js/script.js
+++ src/js/script.js
@@ -3,4 +3,21 @@
  */
 $(document).ready(function () {
     //$('.header-contact-allNumber').find('label:after').css({'backgroundPosition': '-3px 11px', 'backgroundColor':'aqua'});
+    $('.chkBx').on('click', function (e) {
+
+        if ($('#chkBx-allNumber').prop("checked") == true) {
+
+            $('.header-contact-allNumber')
+                .find('.chkBx-showPhone')
+                .removeClass('hide-phone')
+                .addClass('show-phone');
+        } else {
+            $('.header-contact-allNumber')
+                .find('.chkBx-showPhone')
+                .removeClass('show-phone')
+                .addClass('hide-phone');
+        }
+
+    });
+
 });
