0183a17dce6947259594a37aea9bfb6a1db14e8b
Yaroslav Liakh <liakh_ya@mail.ru>
2016-10-20 00:00:17 +0300

    create modal window

--- src/js/script.js
+++ src/js/script.js
@@ -21,18 +21,34 @@ $(document).ready(function () {
 
     });
 
-    function onClickConsult(){
+    function onClickConsult(objModal){
         if($('body').find('div').hasClass('modal-window')){
             $appendModal.show();
         }else{
             $('body').append($appendModal);
-            $('.modal-window').append('<p class="paragraph">' + '</p>');
+            $('.modal-window').append('<div class="paragraph">' + '</div>');
         }
+        $title = $('<p class="paragraph-title">' + objModal.title + '</p>');
+        $inputName = $('<input type="text" class="paragraph-name" placeholder="Ваше имя:">');
+        $inputPhone = $('<input type="text" class="paragraph-phone" placeholder="Ваш телефон:">');
+        $btnModal = $('<button class="btn-custom btn-appointment">' + objModal.btn + '</button>');
+        $('.modal-window').find('div.paragraph').append($title).append($inputName).append($inputPhone).append($btnModal);
     }
 
-    $(".btn-appointment").on('click', onClickConsult);
+    $(".btn-appointment").on('click', onClickConsult, objModalForm.appointments);
     $appendModal.on('click', function(){
         $appendModal.hide();
     });
 
 });
+
+var objModalForm = {
+    appointments:{
+        title:'Записаться на прием',
+        btn:'Подтвердить запись'
+    },
+    consultation:{
+        title:'Задать вопрос врачу',
+        btn:'Задать вопрос'
+    }
+};
