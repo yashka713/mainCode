a92de34bab67747403ec65a628e14c59af027bdd
Yaroslav Liakh <liakh_ya@mail.ru>
2016-10-19 23:31:03 +0300

    create modal window

--- src/js/script.js
+++ src/js/script.js
@@ -3,6 +3,7 @@
  */
 $(document).ready(function () {
     //$('.header-contact-allNumber').find('label:after').css({'backgroundPosition': '-3px 11px', 'backgroundColor':'aqua'});
+    var $appendModal = $('<div class="modal-window">');
     $('.chkBx').on('click', function (e) {
 
         if ($('#chkBx-allNumber').prop("checked") == true) {
@@ -18,6 +19,20 @@ $(document).ready(function () {
                 .addClass('hide-phone');
         }
 
+    });
+
+    function onClickConsult(){
+        if($('body').find('div').hasClass('modal-window')){
+            $appendModal.show();
+        }else{
+            $('body').append($appendModal);
+            $('.modal-window').append('<p class="paragraph">' + '</p>');
+        }
+    }
+
+    $(".btn-appointment").on('click', onClickConsult);
+    $appendModal.on('click', function(){
+        $appendModal.hide();
     });
 
 });
